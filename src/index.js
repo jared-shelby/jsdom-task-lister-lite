ccfee76ab97ca676b909e2b27f3097d4daae3d60
Jared Shelby <jared.shelby@yale.edu>
2022-07-15 00:02:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById("create-task-form");
+form.addEventListener("submit", (event) => {
+    // don't refresh page on submit
+    event.preventDefault();
+
+    // grab what's in the input field
+    const input = document.getElementById("new-task-description").value;
+
+    // grab the task list & append user input as <li>
+    const taskList = document.getElementById("tasks");
+    const newItem = document.createElement("li");
+    newItem.innerHTML = input;
+    taskList.appendChild(newItem);
 });
